5879c7a5b4017945ed23e307d23c1f2a068bb2b0
Alex Evans <aeboston1@gmail.com>
2018-02-09 21:38:09 -0500

    styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180209102452) do
+ActiveRecord::Schema.define(version: 20180210011440) do
+
+  create_table "carts", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
