d20341e6b0c9ba2c5671291d7635b47fd4969298
Lisa Holm <holm.lisa@hotmail.com>
2018-02-11 12:24:21 -0500

    lisas first push showpage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180210192118) do
+ActiveRecord::Schema.define(version: 20180210221603) do
 
   create_table "carts", force: :cascade do |t|
     t.integer "user_id"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20180210192118) do
     t.integer "price_in_cents"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
   end
 
   create_table "users", force: :cascade do |t|
