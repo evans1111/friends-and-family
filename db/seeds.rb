765ee577f9009ee3b979dbec90d01eb6dd0c605b
Lisa Holm <holm.lisa@hotmail.com>
2018-02-12 17:03:42 -0500

    styling show

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 p "Seeding promos"
 PRODUCTS = [
-  { name: 'Watch 1', description: 'Watch description 1', price_in_cents: 179, picture: 'https://cdn.shopify.com/s/files/1/0377/2037/products/GunmetalSandstoneLeather.Front.EDIT_1024x.jpg?v=1510684261'},
+  { name: 'Hello', description: 'hellooo', price_in_cents: 179, picture: 'https://cdn.shopify.com/s/files/1/0377/2037/products/GunmetalSandstoneLeather.Front.EDIT_1024x.jpg?v=1510684261'},
   { name: 'Watch 2', description: 'Watch description 2', price_in_cents: 99, picture: 'https://ae01.alicdn.com/kf/HTB1llr9RXXXXXbxXFXXq6xXFXXXk/No-logo-watches-high-quality-fashion-lovers-watch-customized-women-dress-watch-casual-leather-men-watches.jpg_640x640.jpg'},
   { name: 'Watch 3', description: 'Watch description 3', price_in_cents: 99, picture: 'https://ae01.alicdn.com/kf/HTB1Xc.5PVXXXXbgaVXXq6xXFXXXC/NO-LOGO-classic-Elite-Watch-Men-Women-Simple-Stable-Pointer-Clock-Fashion-Interview-White-Collar-Wrist.jpg_640x640.jpg'},
   { name: 'Watch 4', description: 'Watch description 4', price_in_cents: 99, picture: 'http://doomhall.com/media/2016/03/SKU2737452.jpg'},
